c45936e43a67b5fd6ba47873ba3b09e419f21482
SajavaC <chung7748@gmail.com>
2026-07-06 22:44:01 -0500

    Update Recent_Order_Price.sql

--- queries/Recent_Order_Price.sql
+++ queries/Recent_Order_Price.sql
@@ -1,7 +1,54 @@
-SELECT A.P_Name, A.PO_Date, A.UP_b4T, A.UP, A.SI
-FROM (SELECT P_Name, PO_Date, UP_b4T, UP, SI
-           FROM Products INNER JOIN (Orders INNER JOIN ORD_Det ON Orders.ID = ORD_Det.ORD_ID) ON Products.ID = ORD_Det.Product_ID)  AS A
-WHERE (((A.PO_Date)=(SELECT MAX(B.PO_Date)
-                                    FROM (SELECT P_Name, PO_Date, UP_b4T, UP, SI
-                                                 FROM Products INNER JOIN (Orders INNER JOIN ORD_Det ON Orders.ID = ORD_Det.ORD_ID) ON Products.ID = ORD_Det.Product_ID) as B
-                                    WHERE B.P_Name = A.P_Name)));
+-- ==============================================================================
+-- Description: Advanced Correlated Subquery to dynamically capture the absolute 
+--              latest historical purchase price and calculate custom case costs.
+-- ==============================================================================
+
+SELECT 
+    A.P_Name, 
+    A.PO_Date, 
+    A.UP_b4T, 
+    A.UP, 
+    A.SI, 
+    A.Unit, 
+    ROUND(A.Unit * A.UP, 2) AS CasePrice
+FROM 
+    (
+        SELECT 
+            Products.P_Name, 
+            Orders.PO_Date, 
+            ORD_Det.UP_b4T, 
+            ORD_Det.UP, 
+            ORD_Det.SI, 
+            Products.Unit 
+        FROM 
+            Products 
+            INNER JOIN (
+                Orders 
+                INNER JOIN ORD_Det ON Orders.ID = ORD_Det.ORD_ID
+            ) ON Products.ID = ORD_Det.Product_ID
+    ) AS A
+WHERE 
+    A.PO_Date = (
+        SELECT 
+            MAX(B.PO_Date)     
+        FROM 
+            (
+                SELECT 
+                    Products.P_Name, 
+                    Orders.PO_Date, 
+                    ORD_Det.UP_b4T, 
+                    ORD_Det.UP, 
+                    ORD_Det.SI, 
+                    Products.Unit         
+                FROM 
+                    Products          
+                    INNER JOIN (
+                        Orders          
+                        INNER JOIN ORD_Det ON Orders.ID = ORD_Det.ORD_ID
+                    ) ON Products.ID = ORD_Det.Product_ID     
+            ) AS B     
+        WHERE 
+            B.P_Name = A.P_Name 
+    )
+ORDER BY 
+    A.PO_Date DESC;
