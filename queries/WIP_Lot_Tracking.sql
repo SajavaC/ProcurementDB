b2842bcd54350753ffa21eee1497864f96094772
SajavaC <chung7748@gmail.com>
2026-07-06 22:45:17 -0500

    Create WIP_Lot_Tracking.sql

---
+++ queries/WIP_Lot_Tracking.sql
@@ -0,0 +1,43 @@
+-- ==============================================================================
+-- WORK IN PROGRESS (WIP)
+-- Description: Reconciles total lot quantities received against outbound tracking logs
+--              to calculate live, remaining in-stock balances.
+-- ==============================================================================
+
+SELECT 
+    InnerQuery.P_Name, 
+    InnerQuery.LOT, 
+    InnerQuery.SumOfQty AS TotalQty, 
+    OuterQuery.SumOfQuantity AS OutboundQty, 
+    (InnerQuery.SumOfQty - OuterQuery.SumOfQuantity) AS InStock
+FROM 
+    (
+        SELECT 
+            Products.P_Name, 
+            Lot.LOT, 
+            SUM(Lot.Qty) AS SumOfQty 
+        FROM 
+            Orders 
+            INNER JOIN (
+                Products 
+                INNER JOIN (
+                    ORD_Det 
+                    INNER JOIN (
+                        Freight 
+                        INNER JOIN Lot ON Freight.ID = Lot.Freight_ID
+                    ) ON ORD_Det.ID = Freight.OD_ID
+                ) ON Products.ID = ORD_Det.Product_ID
+            ) ON Orders.ID = ORD_Det.ORD_ID 
+        GROUP BY 
+            Products.P_Name, 
+            Lot.LOT
+    ) AS InnerQuery 
+    LEFT JOIN (
+        SELECT 
+            POTracking.LOT, 
+            SUM(POTracking.Quantity) AS SumOfQuantity 
+        FROM 
+            POTracking 
+        GROUP BY 
+            POTracking.LOT
+    ) AS OuterQuery ON InnerQuery.LOT = OuterQuery.LOT;
